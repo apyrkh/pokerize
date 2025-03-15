a7694bcc88d79afe90e16e9d61f77954e178460c
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2025-03-15 01:04:05 +0100

    Updated db-client

--- src/db/db-client.ts
+++ src/db/db-client.ts
@@ -1,29 +1,28 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './db-types';
 
-const supabase = createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
+var supabase = createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
 
-export const db = {
+export var db = {
   countRooms: async () => {
-    const { count, error } = await supabase.from('room').select('*', { count: 'exact', head: true });
+    var { count, error } = await supabase.from('room').select('*', { count: 'exact', head: true });
     if (error) throw error;
     return count ?? 0;
   },
 
   getRoom: async (id: string) => {
-    const { data, error } = await supabase.from('room').select('*, player(*)').eq('id', id).single();
-    if (error) throw error;
-    return data;
+    var { data, error } = await supabase.from('room').select('*, player(*)').eq('id', id).single();
+    return error ? null : data;
   },
 
   createRoom: async () => {
-    const { data, error } = await supabase.from('room').insert({}).select('*, player(*)').single();
+    var { data, error } = await supabase.from('room').insert({}).select('*, player(*)').single();
     if (error) throw error;
     return data;
   },
 
   getPlayer: async (roomId: string, userId: string) => {
-    const { data, error } = await supabase
+    var { data, error } = await supabase
       .from('player')
       .select('*')
       .eq('room_id', roomId)
@@ -34,7 +33,7 @@ export const db = {
   },
 
   upsertPlayer: async ({ roomId, userId, userName, role = 'USER' }: { roomId: string; userId: string; userName: string; role?: 'USER' | 'VIEWER' }) => {
-    const { data, error } = await supabase
+    var { data, error } = await supabase
       .from('player')
       .upsert([{ room_id: roomId, user_id: userId, user_name: userName, role }], { onConflict: 'room_id, user_id' })
       .select()
