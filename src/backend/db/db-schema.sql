f806afd44f7e4c3bb5454e40de50f6417391ce1d
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2025-03-21 01:59:09 +0100

    Update DB schema - added realtime

--- src/backend/db/db-schema.sql
+++ src/backend/db/db-schema.sql
@@ -1,11 +1,11 @@
--- Cleanup DB
+-- === Drop DB ===
 DROP TABLE IF EXISTS public.vote;
 DROP TABLE IF EXISTS public.player;
 DROP TABLE IF EXISTS public.room;
 
 DROP TYPE IF EXISTS public.player_role;
 
--- Create DB
+-- === Create DB ===
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- public.room
@@ -47,3 +47,14 @@ CREATE TABLE public.vote (
   UNIQUE(room_id, user_id), -- 1 vote per player
   FOREIGN KEY (room_id, user_id) REFERENCES public.player(room_id, user_id) ON DELETE CASCADE
 );
+
+-- Realtime
+ALTER PUBLICATION supabase_realtime ADD TABLE public.room;
+ALTER PUBLICATION supabase_realtime ADD TABLE public.player;
+ALTER PUBLICATION supabase_realtime ADD TABLE public.vote;
+
+-- most likely the following is not needed, but keep it for some time
+-- ALTER TABLE public.room REPLICA IDENTITY FULL;
+-- ALTER TABLE public.player REPLICA IDENTITY FULL;
+-- ALTER TABLE public.vote REPLICA IDENTITY FULL;
+
